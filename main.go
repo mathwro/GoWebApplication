992e6c9e617a00c70c3c244b3839c8b535f73ae7
Mathias Wrobel <mathias@wrobel.dk>
2022-05-10 00:36:39 +0200

    Added inline comments and fixed formatting of echo string

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ var mutex = &sync.Mutex{}
 
 // Function to echo the request
 func echoString (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello %s", strings.TrimPrefix(r.URL.Path, "/"))
+	var echoString = strings.TrimLeft(r.URL.Path, "/echo/")
+	fmt.Fprintf(w, "hello %s", echoString)
 }
 
 // Function to increment the counter
@@ -32,16 +33,20 @@ func incrementCounter (w http.ResponseWriter, r *http.Request) {
 
 
 func main() {
-	//http.HandleFunc("/", echoString)
+	// Serve static website
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	
+	// Servce echo function
+	http.HandleFunc("/echo/", echoString)
 
+	// Serve increment function
 	http.HandleFunc("/increment", incrementCounter)
 
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-
+	// Serve the generic hi message
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hi")
     })
 
+	// Start the server
 	log.Fatal(http.ListenAndServe("localhost:8081", nil))
-
 }
